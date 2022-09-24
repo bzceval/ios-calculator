112905bececeaf746c2db5b2f0446a86b87a6a19
bzceval <busraaceval@gmail.com>
2022-09-24 14:02:50 +0300

    created first version functionality

--- js/app.js
+++ js/app.js
@@ -1,87 +1,64 @@
-// console.log("******app.js bağlandı****")
+const container = document.querySelector(".container");
+let result1 = document.querySelector("#result1");
+let result2 = document.querySelector("#result2");
 
-// Selectors
-let firstResult = document.querySelector("#firstResult");
-let secondResult = document.querySelector("#secondResult");
-let numbers = document.querySelector(".numbers");
-
-// Values From the User
-let number1 = "";
-let number2 = "";
+let num1 = "";
+let num2 = "";
 let operator = "";
 
-//Event Control
-numbers.addEventListener("click", (event) => {
-  // console.log(event.target)
-  //number event
-  if (event.target.className == "number") {
-    // console.log("number");
-    number2 += event.target.innerText;
-    secondResult.innerText = number2;
-  }
-  // opator event
-  else if (event.target.className == "oparator") {
-    // console.log("oparator");
-    if (number1) {
-      if (!number2) {
+container.addEventListener("click", (event) => {
+  if (event.target.classList.contains("number")) {
+    num2 += event.target.innerText;
+    result1.innerText = num2;
+  } else if (event.target.classList.contains("operator-ac")) {
+    reset();
+  } else if (event.target.classList.contains("operator")) {
+    if (num1) {
+      if (!num2) {
         alert("Enter a number!");
       } else {
-        number1 = calculate(Number(number1), operator, Number(number2));
-        if (String(number1).slice(String(number1).indexOf(".")).length > 6) {
-          number1 = number1.toFixed(6);
+        num1 = calculate(Number(num1), operator, Number(num2));
+        if (String(num1).slice(String(num1).indexOf(".")).length > 6) {
+          num1 = num1.toFixed(6);
         }
         operator = event.target.innerText;
-        secondResult.innerText = number1 + " " + operator;
-        firstResult.innerText = "";
-        number2 = "";
+        result2.innerText = num1 + " " + operator;
+        result1.innerText = "";
+        num2 = "";
       }
     } else {
-      number2 = firstResult.innerText;
-      number1 = number2;
-      number2 = "";
+      num2 = result1.innerText;
+      num1 = num2;
+      num2 = "";
       operator = event.target.innerText;
-      secondResult.innerText = number1 + " " + operator;
-      firstResult.innerText = "";
+      result2.innerText = num1 + " " + operator;
+      result1.innerText = "";
     }
+  } else if (event.target.classList.contains("equal")) {
+    num2 = calculate(Number(num1), operator, Number(num2));
+    if (String(num2).length > 6) {
+      num2 = num2.toFixed(6);
+    }
+    result1.innerText = num2;
+    result2.innerText = "";
+    num1 = "";
+    num2 = "";
   }
-  // equal event
-  else if (event.target.classList.contains("equal")) {
-    console.log("equal");
+  if (event.target.id === "minus-plus") {
+    num2 *= -1;
+    result1.innerText = num2;
   }
-  // percent event
-  else if (event.target.classList.contains("percent")) {
-    // console.log("percent");
-    number2 /= 100;
-    secondResult.innerHTML = number2;
-  }
-  // ac-calcutor event
-  else if (event.target.classList.contains("ac-oparator")) {
-    // console.log("ac-oparator");
-    reset();
-  }
-  // minus-plus event
-  else if (event.target.classList.contains("minus-plus")) {
-    // console.log("minus-plus");
-    number2 *= -1;
-    firstResult.innerText = number2;
+  if (event.target.id === "percent") {
+    num2 /= 100;
+    result1.innerText = num2;
   }
 });
 
-// Reset Function
-function reset() {
-  let number1 = "";
-  let number2 = "";
-  let operator = "";
-  firstResult.innerText = "";
-  secondResult.innerText = "";
-}
-
-// Calculate Function
-function calculate(n1, operator, n2) {
-  if (operator === "+") return n1 + n2;
-  else if (opartor === "-") return n1 - n2;
-  else if (oparator === "x") return n1 * n2;
-  else if (oparator === "/") {
+function calculate(n1, oper, n2) {
+  if (oper === "+") return n1 + n2;
+  else if (oper === "-") return n1 - n2;
+  else if (oper === "x") return n1 * n2;
+  else if (oper === "÷") {
     if (!n2) {
       return "Error";
     } else {
@@ -89,3 +66,13 @@ function calculate(n1, operator, n2) {
     }
   }
 }
+
+function reset() {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  result1.innerText = "";
+  result2.innerText = "";
+}
+
+console.log(calculate(3, "+", 4));
